8349e93a8bf647628a46a829f14ca880805cb37d
xzy <2894702202@qq.com>
2024-05-27 10:44:37 +0800

    feat：新增建表语句

---
+++ sql/creat_table.sql
@@ -0,0 +1,43 @@
+-- 创建User表
+create table user
+(
+    username     varchar(256)                       null comment '用户昵称',
+    id           bigint auto_increment comment 'id'
+        primary key,
+    userAccount  varchar(256)                       null comment '账号',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
+    gender       tinyint                            null comment '性别',
+    userPassword varchar(512)                       not null comment '密码',
+    phone        varchar(128)                       null comment '电话',
+    email        varchar(512)                       null comment '邮箱',
+    userStatus   int      default 0                 not null comment '状态 0 - 正常',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
+    planetCode   varchar(512)                       null comment '星球编号',
+    tags         varchar(1024)                      null comment '标签 json 列表'
+)
+    comment '用户';
+
+
+
+-- 创建tag表
+create table tag
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    tagName    varchar(256)                       null comment '标签名称',
+    userId     bigint                             null comment '用户 id',
+    parentId   bigint                             null comment '父标签 id',
+    isParent   tinyint                            null comment '0 - 不是, 1 - 父标签',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    constraint uniIdx_tagName
+        unique (tagName)
+)
+    comment '标签';
+
+create index idx_userId
+    on tag (userId);
